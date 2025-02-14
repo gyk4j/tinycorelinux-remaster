e542f3f72a2e4be00c31a3fc686daa4eb3d3b854
gyk4j <147011991+gyk4j@users.noreply.github.com>
2025-02-14 09:17:22 +0800

    Add custom boot code checks

---
+++ bin/7-bootcodes.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+##############################################################################
+#                            License & Copyright                             #
+#                                                                            #
+# The materials herein are from "Into the Core: A look at Tiny Core Linux"   #
+# (http://www.tinycorelinux.net/book.html) by Lauri Kasanen et al. The book  #
+# is licensed under the CC BY-NC-SA 4.0 license (http://creativecommons.org/ #
+# licenses/by-nc-sa/4.0/). (C) 2013 Lauri Kasanen.                           #
+##############################################################################
+
+cat ../home/tc/.profile
